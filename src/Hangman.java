20d766f51dee9e7c0e0de4add527306ff7f83f3a
Gregory C <cgregg@belgacom.net>
2024-04-22 12:44:47 +0200

    add loop continue

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,22 +1,40 @@
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
-        
-        String motMystere[] = {"ORANGE"};
-        String mot; 
+
+        String motMystere[] = { "ORANGE" };
+        String mot;
         char tabMot[];
         char tabMotTrouve[];
+        char choixRejouer;
+        boolean rejouer = true;
 
         myUtil.bienvenue();
 
-        mot = motMystere[0];
-        tabMot = mot.toCharArray();
+        do {
 
+            // mot devient un string du mot mystere
+            mot = motMystere[0];
+            // tabmot passe le string en chaine de caractères
+            tabMot = mot.toCharArray();
 
-        tabMotTrouve =  myUtil.initialiserTabMotTrouve(tabMot);
-        System.out.println(tabMotTrouve);
-        System.out.println(tabMot);
+            tabMotTrouve = myUtil.initialiserTabMotTrouve(tabMot);
+            System.out.println();
 
-        myUtil.trouverMotMystere(tabMot, tabMotTrouve);
+            myUtil.trouverMotMystere(tabMot, tabMotTrouve);
+            System.out.println();
+
+            System.out.println("Press c to continue or another letter to exit");
+            choixRejouer = new Scanner(System.in).nextLine().charAt(0);
+            if (choixRejouer == 'c' || choixRejouer == 'C') {
+                rejouer = true;
+            } else {
+                rejouer = false;
+                System.out.println("\nthank you for participating ;-)\n");
+            }
+
+        } while (rejouer);
 
     }
 }
